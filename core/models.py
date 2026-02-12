a00e71062b01e1a1b72eb6a769edee64bfccf957
Bruna Camilotto <92326961+BrunaCamilotto@users.noreply.github.com>
2026-02-12 17:11:30 -0300

    Inclusão DB

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    preco = models.DecimalField(decimal_places=2, max_digits=8)
+    descricao = models.CharField(max_length=200)
+    quantidade = models.PositiveIntegerField(default=0)
+    data_de_validade = models.DateField(auto_now_add=True)
