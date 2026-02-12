c010e03a37c6c7a0cdc962ebb429bea80b01c84e
Bruna Camilotto <92326961+BrunaCamilotto@users.noreply.github.com>
2026-02-12 16:55:05 -0300

    Inclusão e configuração das paginas Index e Contatos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {'curso': 'Desenvolvimento de Sistemas' }
+    return render(request, 'index.html', context)
+
+def contatos(request):
+    context = {'nome': 'Bruna' }
+    return render(request, 'contatos.html', context)
+
